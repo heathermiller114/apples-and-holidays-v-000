a1e5f3d8cd83ae689d27f08bd4f36b7df35fa738
Heather Miller <heather.miller114@gmail.com>
2018-09-09 21:29:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       holidays_capitalize = holiday.to_s.split("_")
-      holidays_capitlize.each do |day|
+      holidays_capitalize.each do |day|
         day.capitalize!
       end
       holidays_capitalize.join
