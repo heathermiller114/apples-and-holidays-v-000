481ffdcfd7f5102bef53841ceb6fdd38c3dc30e3
Heather Miller <heather.miller114@gmail.com>
2018-09-09 22:12:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if holiday.
+      if holiday.has_value("BBQ")
 end
 
 
